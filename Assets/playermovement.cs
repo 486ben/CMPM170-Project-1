52cf3a8bd5ae7f5dc65cc0c65b47e31279133ffe
486ben <zhuan152@ucsc.edu>
2023-10-15 19:46:44 -0700

    Add new feature and update documentation

--- Assets/playermovement.cs
+++ Assets/playermovement.cs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -28,3 +29,35 @@ public class playermovement : MonoBehaviour
         }
     }
 }
+=======
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playermovement : MonoBehaviour
+{
+    Rigidbody rb;
+    [SerializeField] float movementspeed = 5f;
+    [SerializeField] float jumpforce = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        rb.velocity = new Vector3(horizontalInput * movementspeed, rb.velocity.y, verticalInput * movementspeed);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.velocity = new Vector3(rb.velocity.x, jumpforce, rb.velocity.z);
+        }
+    }
+}
+>>>>>>> f743f06 (first update)
